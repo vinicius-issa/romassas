1e98e35548fccba4c8fb540f1db115d30bb16cd7
Vinicius Issa <vinicius.almeidaissa@gmail.com>
2020-06-23 14:17:13 -0300

    [Refat]App

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,13 @@ import { Container } from '@material-ui/core';
 import Route from './routes'
 import './App.css'
 import Alert from './components/elements/Alert'
-
+import AppBar from './components/elements/Menu'
 
 
 const App: React.FC<{}> = ()=> {
   return (
     <div className="App">
+      <AppBar />
       <Container >
         <Alert />
         <Route/>
--- src/index.tsx
+++ src/index.tsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import dotenv from 'dotenv';
 import App from './App';
-import AppBar from './components/elements/Menu'
 import { Provider } from 'react-redux';
 
 import configStore from './store/storeConfig';
 
 const store = configStore();
 
-dotenv.config();
 
 ReactDOM.render(
   <Provider store={store}>
     <React.StrictMode>
-      <AppBar />
        <App />
     </React.StrictMode>
   </Provider>,
